953c88ff91ec585475d83a8fdbee8fbb055131d9
”Litera-til“git config --global user.email 1459676432@qq.com” <459676432@qq.com>
2019-03-08 18:31:05 +0800

    Revert "大部分代码重构，新增爬取到数据库保存"
    This reverts commit 8fcbb2687d435fe8dcc06d2bb77dbbf866e129a9.

--- src/main/java/top/ratil/animecrawler/api/ResultCode.java
+++
@@ -1,9 +0,0 @@
-package top.ratil.animecrawler.api;
-
-public class ResultCode {
-    public static final Integer SUCCESS = 200;
-    public static final Integer FAIL = 400;
-    public static final Integer UNAUTHORIZED = 401;
-    public static final Integer NOT_FOUND = 404;
-    public static final Integer INTEGER_SERVER_ERROR = 500;
-}
--- src/main/java/top/ratil/animecrawler/api/status/PageStatus.java
+++
@@ -1,8 +0,0 @@
-package top.ratil.animecrawler.api.status;
-
-public class PageStatus {
-    /**
-     * 用来保存请求网页后返回的status code
-     */
-    public static int PAGE_STATUS = 404;
-}
